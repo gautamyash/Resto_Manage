4ac7cc194c145ed08759e053e613e7f9578ed16e
gautamyash <gautamyash37@gmail.com>
2024-09-17 16:43:45 +0530

    first commit

---
+++ src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {images} from"../../constants"
+
+const SubHeading = () => (
+  <div style={{marginBottom:"1rem"}}>
+    <p className='p__cormorant'>
+      <img src={images.spoon} />
+    </p>
+  </div>
+);
+
+export default SubHeading;
